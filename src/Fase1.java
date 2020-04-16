3cff83f9995345e7ac84153f41dd5e9580f3ec54
cotoflux <guaupelu@hotmail.com>
2020-04-16 10:04:32 +0200

    Finalizado fase 1

---
+++ src/Fase1.java
@@ -0,0 +1,21 @@
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+		
+		
+		char [] nom = new char[5];
+		
+		nom[0]='N';
+		nom[1]='u';
+		nom[2]='r';
+		nom[3]='i';
+		nom[4]='a';
+		
+		for(int i=0; i<5; i++) {
+			System.out.print(nom[i]+" ");
+		}
+
+	}
+
+}
