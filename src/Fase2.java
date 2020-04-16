6b1e6a5364248555dccfc503bd6d3c52b95e6d19
cotoflux <guaupelu@hotmail.com>
2020-04-16 12:21:26 +0200

    Añadido parte del programa de imprimir vocales y consonantes, FAse 2 terminada

--- src/Fase2.java
+++ src/Fase2.java
@@ -9,6 +9,7 @@ public class Fase2 {
 		char nombre;
 		
 		List<Character> nom = new ArrayList<Character>();
+		int i;
 		
 		nom.add('N');
 		nom.add('u');
@@ -16,11 +17,20 @@ public class Fase2 {
 		nom.add('i');
 		nom.add('a');
 		
-		for (int i = 0; i < nom.size(); i++) {
+		for (i = 0; i < nom.size(); i++) {
 			nombre = nom.get(i);
 			System.out.print(nombre +" ");
+			
+			if(nom.get(i) == 'a'|| nom.get(i) == 'e' || nom.get(i) == 'i'|| nom.get(i) == 'o'||nom.get(i) == 'u' ) {
+				System.out.println( nom.get(i) + "Soy una VOCAL");
+				} else if (nom.get(i) != 'a'|| nom.get(i) != 'e' || nom.get(i) != 'i'|| nom.get(i) != 'o'||nom.get(i) != 'u' ){
+				System.out.println("Soy una CONSONANTE");
+				}else {
+				System.out.println("Els noms de persones no contenen números!");
+				}
+			}
 		}
-		
-	}
+				
 
 }
+		
